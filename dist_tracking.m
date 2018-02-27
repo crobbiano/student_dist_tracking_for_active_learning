57aec940bf627cf59a3b88e567510a85f4bf2f17
Chris <iamchris@rams.colostate.edu>
2018-02-27 10:38:52 -0700

    Added some comments to the header. Need to add actual code

---
+++ dist_tracking.m
@@ -0,0 +1,11 @@
+%% New script for identifying and splitting students into groups based on
+%  their # of standard deviations away from the mean at the end of the 
+%  sophomore year.  This should be done at the GPA level.
+%
+%  Want to then track those groups of students in each of the junior year
+%  ECE courses and watch how their distributions change from the sophomore
+%  year to the junior year.
+%
+%  Then compare the changes in these distributions from sophomore year to 
+%  junior year for both the students that are pre-KI and post-KI and identify
+%  if any differences have occured due to the introduction of the KI.
